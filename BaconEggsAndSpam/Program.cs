6e126817dfed9e4c3c73c374fcb0e05c3cc65b95
k-sten <stenen@gmail.com>
2020-03-16 16:36:34 +0100

    Add project files.

---
+++ BaconEggsAndSpam/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace BaconEggsAndSpam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputfile = System.IO.File.ReadAllText(@"input.txt");
+            string[] input = inputfile.Split("\n");
+
+            Console.WriteLine(input[0]);
+
+            Array.Sort<string>(input);
+
+            foreach (var line in input)
+            {
+                Console.WriteLine(line);
+            }
+
+            inputfile.
+        }
+    }
+}
